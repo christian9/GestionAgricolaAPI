b18983ab0ee3d78224e540ebc22e6a59d7516662
David Solano <desolanom@gmail.com>
2017-03-17 10:04:04 -0600

    Start command

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var Hapi = require('hapi');
 var app = require('./routes/app');
 
-var port = process.env.PORT || 3000;
-var host = process.env.HOST || 'http://gestionagricolaapi.azurewebsites.net';
+var port = process.env.PORT || 5000;
+var host = process.env.HOST || 'localhost';
 var server = new Hapi.Server(host,port,{ cors: true });
 app.routes(server);
 
